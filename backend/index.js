deadb318451f4916f2277f867b960632d47bda9d
Himanshu <95962613+Himanshu0316@users.noreply.github.com>
2023-07-14 13:42:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,7 @@ const corsOptions ={
     credentials:true,
 }
 app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
 app.use("/seat",seatRouter)
